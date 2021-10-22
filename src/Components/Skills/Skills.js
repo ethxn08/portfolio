b7a7e1513ac33e3ea34f12643965ac888e4e6560
ethxn08 <ethanalfaro08@gmail.com>
2021-10-22 21:24:36 +0200

    Skills Cards

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,22 +1,71 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.scss";
+
+//Images
+import Bootstrap_logo from "../../assets/icons/Bootstrap_logo.svg.png";
+import nodejs_logo from "../../assets/icons/nodejs-logo.png";
+import js_logo from "../../assets/icons/js-logo.png";
+import react_logo from "../../assets/icons/react_logo.png";
+import redux_logo from "../../assets/icons/redux_logo.png";
+
 function Skills() {
   return (
     <div className="skills">
       <h1>Skills</h1>
       <Swiper
-        spaceBetween={0}
+        spaceBetween={10}
         slidesPerView={3}
         onSlideChange={() => {
           console.log("slide change");
         }}
         onSwiper={(swiper) => console.log(swiper)}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide className="slide">
+          <div className="skill-card">
+            <div className="skill-image">
+              <img src={Bootstrap_logo} alt="bootstrap" />
+            </div>
+            <div className="skill-title">Bootstrap</div>
+            <div className="skill-level">10/10</div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="slide">
+          <div className="skill-card">
+            <div className="skill-image">
+              <img src={nodejs_logo} alt="node" />
+            </div>
+            <div className="skill-title">Node Js</div>
+            <div className="skill-level">10/10</div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="slide">
+          <div className="skill-card">
+            <div className="skill-image">
+              <img src={js_logo} alt="" />
+            </div>
+            <div className="skill-title">Java Script</div>
+            <div className="skill-level">10/10</div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="slide">
+          <div className="skill-card">
+            <div className="skill-image">
+              <img src={react_logo} alt="" />
+            </div>
+            <div className="skill-title">React</div>
+            <div className="skill-level">10/10</div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="slide">
+          <div className="skill-card">
+            <div className="skill-image">
+              <img src={redux_logo} alt="" />
+            </div>
+            <div className="skill-title">Redux</div>
+            <div className="skill-level">10/10</div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
