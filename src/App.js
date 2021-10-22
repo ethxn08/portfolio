d08f8ee039562eb7c4dbe1ff34e568683d48ad4e
ethxn08 <ethanalfaro08@gmail.com>
2021-10-22 20:22:32 +0200

    Swiper Implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header";
 import AboutMe from "./Components/AboutMe";
+import Skills from "./Components/Skills";
+
 function App() {
   return (
     <div className="App">
       <Header />
       <AboutMe />
+      <Skills />
     </div>
   );
 }
