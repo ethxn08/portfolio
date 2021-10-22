6dc46a2bf7686c8d264bbddecb22d5e5a3ba992c
ethxn08 <ethanalfaro08@gmail.com>
2021-10-22 19:18:57 +0200

    Header and About Me Structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./Components/Header";
+import AboutMe from "./Components/AboutMe";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AboutMe />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/AboutMe/AboutMe.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+function AboutMe() {
+  return (
+    <div className="about-me">
+      <h1>About Me</h1>
+      <div className="about-me-container">
+        <div className="about-me-paragraph">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius non,
+            tortor, amet sed. Suscipit mi blandit eu elit pharetra fermentum.
+            Nunc cum porttitor lorem placerat sed nec. In in vel at egestas
+            morbi dictum. Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit. Varius non, tortor, amet sed. Suscipit mi blandit eu elit
+            pharetra fermentum. Nunc cum porttitor lorem placerat sed nec. In in
+            vel at egestas morbi dictum. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Varius non, tortor, amet sed. Suscipit mi blandit
+            eu elit pharetra fermentum. Nunc cum porttitor lorem placerat sed
+            nec. In in vel at egestas morbi dictum.
+          </p>
+        </div>
+        <div className="about-me-paragraph">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius non,
+            tortor, amet sed. Suscipit mi blandit eu elit pharetra fermentum.
+            Nunc cum porttitor lorem placerat sed nec. In in vel at egestas
+            morbi dictum. Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit. Varius non, tortor, amet sed. Suscipit mi blandit eu elit
+            pharetra fermentum. Nunc cum porttitor lorem placerat sed nec. In in
+            vel at egestas morbi dictum. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Varius non, tortor, amet sed. Suscipit mi blandit
+            eu elit pharetra fermentum. Nunc cum porttitor lorem placerat sed
+            nec. In in vel at egestas morbi dictum.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
