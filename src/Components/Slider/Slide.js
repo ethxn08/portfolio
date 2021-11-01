9a4fd956a538f13959560e75686dfa9428391e66
ethxn08 <ethanalfaro08@gmail.com>
2021-11-01 17:33:41 +0100

    Portfolio

--- src/Components/Slider/Slide.js
+++ src/Components/Slider/Slide.js
@@ -22,7 +22,7 @@ const slideInfo = [
 
 const slides = slideInfo.map((slide) => (
     <div className="slide-container">
-        <a href={slide.url} target="_blank" rel="noopener">
+        <a href={slide.url} target="_blank" rel="noreferrer">
         <img src={slide.src} alt={slide.alt} />
         <div className="slide-desc">
             <span>{slide.desc}</span>
