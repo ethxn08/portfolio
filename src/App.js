2dcd5e534a30fd55d150ce6e1e67553e1ec79fa8
ethxn08 <ethanalfaro08@gmail.com>
2021-11-02 16:53:20 +0100

    Loading Component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,25 @@ import About from './Components/About';
 import Slider from './Components/Slider';
 import Info from "./Components/Info";
 import Footer from './Components/Footer';
+import {Spinner} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
+
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    },3000)
+  }, [])
+
 
   const [scrollHeight, setScrollHeight] = useState(0)
 
   const handleScroll = () =>{
     const position = window.pageYOffset;
-    setScrollHeight(position)
+    setScrollHeight(position) 
   }
 
   useEffect(() => {
@@ -20,12 +32,24 @@ function App() {
   }, [scrollHeight])
   return (
     <div className="App">
+      {
+        loading ? 
+        <div className="loading">
+          <h1>A second, please. </h1>
+          <h3>The page is being configured for you, ...</h3>
+        <Spinner color="light" />
+        </div>
+        : 
+        <>
         <Navbar isScrolling={scrollHeight} />
         <Cover />
         <About />
         <Slider />
         <Info />
         <Footer />
+        </>
+      }
+        
     </div>
   );
 }
